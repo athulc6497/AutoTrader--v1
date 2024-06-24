6eed81ef1b49dbcf08325ee96cfb56163ea531cc
AthulC <athulcaugustian@gmail.com>
2024-06-24 08:51:20 +0530

    .

---
+++ AutoTrader.Web/Components/Pages/CarDetailView.cs
@@ -0,0 +1,21 @@
+﻿using AutoTrader.Models.CarInfo;
+using AutoTrader.Web.Services;
+using Microsoft.AspNetCore.Components;
+
+namespace Cars.Web.Components.Pages
+{
+    public class CarDetailViewBase:ComponentBase
+    {
+        public CarDetails CarInformation { get; set; }=new CarDetails();
+
+        [Inject]
+        public ICarListService CarListService { get; set; }
+
+        [Parameter]
+        public string Id { get; set; }
+        protected async override Task OnInitializedAsync()
+        {
+           CarInformation= await CarListService.GetCar(int.Parse(Id));
+        }
+    }
+}
